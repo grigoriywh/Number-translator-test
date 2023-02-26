56d41eda511faffc3c28651fda0ac427b26764f0
Gregory <42639720+grigoriywh@users.noreply.github.com>
2023-02-26 21:39:58 +0300

    Initial version 1.0

---
+++ GDV1/GDV1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace GDV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button5.Location = new System.Drawing.Point(248, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(195, 40);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Очистить 2 с.с.";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Clear2_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 77);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(193, 40);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Очистить 10 с.с.";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Число в 10 с.с.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 104);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(449, 139);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Основной функционал ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(437, 40);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 29);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 51);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 30);
+            this.textBox2.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Результат работы алгоритма - число в 2 с.с.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 258);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Number Translator v 1.0";
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GDV1/GDV1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace GDV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        //		Кнопки 
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void Clear2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        //	Форма - Выбор директории
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                string num = textBox1.Text;
+                int round = 5;
+   
+                string result = ""; //Результат
+                int left = 0; //Целая часть
+                int right = 0; //Дробная часть
+                string[] temp1 = num.Split(new char[] { '.', ',' }); //Нужна для разделения целой и дробной частей
+                left = Convert.ToInt32(temp1[0]);
+                //Проверяем имеется ли у нас дробная часть
+                if (temp1.Count() > 1)
+                {
+                    right = Convert.ToInt32(num.Split(new char[] { '.', ',' })[1]); //Дробная часть
+                }
+                //Алгоритм перевода целой части в двоичную систему
+                while (true)
+                {
+                    result += left % 2; //В ответ помещаем остаток от деления. В конце программы мы перевернём строку, так как в обратном порядке записываются остатки
+                    left = left / 2; //Так как Left целое число, то при делении например числа 2359 на 2, мы получим не 1179,5 а 1179
+                    if (left == 0)
+                        break;
+                }
+                result = new string(result.ToCharArray().Reverse().ToArray()); //Реверсирование строки
+                /*Не углублялся в ситуацию, но вдруг при реверсе появятся первые символы нули, а ведь их мы не пишем!
+                Не знаю есть ли необходимость в этом цикле */
+                while (true)
+                {
+                    int i = 0;
+                    if (result[i] == '0')
+                        result = result.Remove(i, 1);
+                    else break;
+                }
+                //Прокеряем есть ли у нас дробная часть, можно было бы проверить и так if(temp1.count()>1)
+                if (right == 0)
+                    textBox2.Text = result;
+
+                //Добавляем разделить целой части от дробной
+                result += '.';
+
+                int count = right.ToString().Count(); // Нам нужно знать кол-во цифр, при превышении которого дописывается единичка
+
+                for (int i = 0; i < round; i++)
+                {
+                    /*Умножаем число на 2 и проверяем, стало ли оно больше по количеству цифр, если да,
+                    то в результат идёт "1" и первая цифра у right удаляется */
+                    right = right * 2;
+                    if (right.ToString().Count() > count)
+                    {
+                        string buf = right.ToString();
+                        buf = buf.Remove(0, 1);
+                        right = Convert.ToInt32(buf);
+
+                        result += '1';
+                    }
+                    else
+                    {
+                        result += '0';
+                    }
+                }
+                textBox2.Text = result;
+
+            }
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        //	Пустые функции 
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
